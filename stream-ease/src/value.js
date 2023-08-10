7721651cffe97456d253ec831de5e3a7bc7975da
VTNQ <votannamquoc@gmail.com>
2023-08-10 10:34:31 +0700

    Create Bootstrap

---
+++ stream-ease/src/value.js
@@ -0,0 +1,68 @@
+let brand=[
+    {
+        id:1,
+        name:'Netflix',
+        img:'',
+        GoldenPlan:'30$/year',
+        DiamondPlan:'60$/year',
+        PlatinumPlan:'70$/year'
+    },
+    {
+        id: 2,
+        name:"Amazon Prime",
+        img:'',
+        GoldenPlan:'33$/6 month',
+        DiamondPlan:'23$/month',
+        PlatinumPlan:'70$/year'
+    },
+    {
+        id: 3,
+        name:"HBO Max",
+        img:'',
+        GoldenPlan:'23$/6 month',
+        DiamondPlan:'15$/month',
+        PlatinumPlan:'75$/year'
+    },
+    {
+        id: 4,
+        name:"Disney+",
+        img:'',
+        GoldenPlan:'23$/6 month',
+        DiamondPlan:'20$/month',
+        PlatinumPlan:'83$/year'
+    },
+    {
+        id: 5,
+        name:'VieOn',
+        img:'',
+        GoldenPlan:'3$/month',
+        DiamondPlan:'13$/3 month',
+        PlatinumPlan:'43$/year'
+    },
+    {
+        id: 6,
+        name:'FPT Play',
+        img:'',
+        GoldenPlan:'1$/month',
+        DiamondPlan:'3$/month',
+        PlatinumPlan:'33$/year'
+    },
+    {
+        id: 7,
+        name:'Hulu',
+        img:'',
+        GoldenPlan:'13$/month',
+        DiamondPlan:'15$/month',
+        PlatinumPlan:'75$/year'
+    },
+    {
+        id: 8,
+        name:'Apple Tv',
+        img:'',
+        GoldenPlan:'23$/6 month',
+        DiamondPlan:'3$ /month',
+        PlatinumPlan:'78$/year'
+    }
+];
+let actor=[];
+let movie=[];
